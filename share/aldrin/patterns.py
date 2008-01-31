8605bf1cee1e5c97917d71ac874282e2fb8d880b
jms <none@none>
2008-01-31 23:55:57 +0100

    [svn] redraw changing parameters in pattern view during automation
    --HG--
    branch : trunk

--- share/aldrin/patterns.py
+++ share/aldrin/patterns.py
@@ -448,6 +448,12 @@ class PatternPanel(gtk.VBox):
 			self.update_all()
 		elif data.type == zzub.zzub_event_type_new_plugin:
 			self.update_all()
+		elif data.type == zzub.zzub_event_type_parameter_changed and player.get_automation(): 
+			try: 
+				self.view.update_line(player.get_position()) 
+				self.view.redraw() 
+			except TypeError:
+				pass 
 
 	def reset(self):
 		"""
