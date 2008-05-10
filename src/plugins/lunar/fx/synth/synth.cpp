2f83b8a5f8a8c5e7099e6ac5aabf51f7df7937fa
tzhau <none@none>
2008-05-10 10:04:41 +0000

    Added pitch slide to minisynth. Increased precision of pitchlfo depth.

--- src/plugins/lunar/fx/synth/synth.cpp
+++ src/plugins/lunar/fx/synth/synth.cpp
@@ -9,6 +9,7 @@
 #define MAX_TRACKS 8
 #define MAX_WAVES 16
 #define TABSIZE 2048
+//#define TABSIZE 8192
 #define POWTABSIZE 8192
 
 class synth : public lunar::fx<synth> {
@@ -21,20 +22,24 @@ public:
 	float scutoff, sreso, scutoff2;
 	float amp;
 
-  struct lfo {
-    float phase;
-    float phasestep;
-    float amp;
-    
-    lfo() {
-      phase = 0.0f;
-      phasestep = 0.0f;
-      amp = 0.0f;
-    }
-  };
+struct lfo {
+	float phase;
+	float phasestep;
+	float amp;
+
+lfo() {
+	phase = 0.0f;
+	phasestep = 0.0f;
+	amp = 0.0f;
+	}
+};
 
 	struct voice {
 		float powtab[POWTABSIZE];
+		float start_phasestep;
+		float end_phasestep;
+		float slide_step;
+		float slide_length;
 		float phasestep;
 		float phase;
 		float freq;
@@ -43,7 +48,7 @@ public:
 		float cutoff;
 		float volume;
 		float *curtab;
-    lfo pitch;
+		lfo pitch;
 		
 		inline void update_powtab(float scutoff) {
 			float c = min(cutoff+freq,20000);
@@ -54,6 +59,9 @@ public:
 		
 		voice() {
 			freq = 0.0f;
+			start_phasestep = 0.0f;
+			end_phasestep = 0.0f;
+			slide_step = 0.0f;
 			phasestep = 0.0f;
 			phase = 0.0f;
 			cutoff = 0.0f;
@@ -95,7 +103,6 @@ public:
 			sawtab[i] = 1.0f - (float(i) / float(TABSIZE))*2.0f;
 			sqrtab[i] = ((i < (TABSIZE / 2)) ? -1.0f : 1.0f );
 			sintab[i] = sin(-M_PI + (M_PI * (float(i*2) / float(TABSIZE-1))));
-//			printf("I=%d: SAW=%f SQR=%f SIN=%f\n", i, sawtab[i], sqrtab[i], sintab[i]);
 		}
 	}
 
@@ -162,12 +169,17 @@ public:
 		}
 		
 		for (int t = 0; t < track_count; ++t) {
-      if (globals->plfospeed) {
-        voices[t].pitch.phasestep = ((*globals->plfospeed)*float(TABSIZE)) / float(transport->samples_per_second);
-      }
-      if (globals->plfodepth) {
-        voices[t].pitch.amp = (*globals->plfodepth) * 12.0f / 100.0f;
-      }
+			if (globals->plfospeed) {
+				voices[t].pitch.phasestep = ((*globals->plfospeed)*float(TABSIZE)) / float(transport->samples_per_second);
+			}
+			if (globals->plfodepth) {
+				voices[t].pitch.amp = (*globals->plfodepth) * 12.0f / 100.0f;
+			}
+			if (globals->pitchslide) {
+				voices[t].slide_length = (*globals->pitchslide) / 1000.0f;
+				voices[t].slide_step = 1 / voices[t].slide_length * 
+						(voices[t].end_phasestep - voices[t].start_phasestep) / float(transport->samples_per_second);
+			}
 			if (tracks[t].note) {
 				if (*tracks[t].note == 0.0f) {
 					voices[t].a.off();
@@ -175,7 +187,15 @@ public:
 					voices[t].a.on();
 					voices[t].volume = 1.0f;
 					voices[t].freq = *tracks[t].note;
-					voices[t].phasestep = ((*tracks[t].note)*float(TABSIZE)) / float(transport->samples_per_second);
+					voices[t].start_phasestep = voices[t].phasestep;
+					voices[t].end_phasestep = ((*tracks[t].note)*float(TABSIZE)) / float(transport->samples_per_second);
+					// no sliding at start
+					if (voices[t].phasestep == 0.0f) {
+						voices[t].start_phasestep = voices[t].end_phasestep;
+						voices[t].phasestep = voices[t].end_phasestep;
+					}
+					voices[t].slide_step = 1 / voices[t].slide_length * 
+						(voices[t].end_phasestep - voices[t].start_phasestep) / float(transport->samples_per_second);
 					voices[t].phase = 0.0f;
 					voices[t].cutoff = scutoff2;
 					voices[t].update_powtab(scutoff * 2.0f);          
@@ -183,8 +203,8 @@ public:
 			}
 			if (tracks[t].volume) {
 				voices[t].volume = float(*tracks[t].volume) / 128.0f;
-        if (voices[t].volume == 1.0f)
-          voices[t].pitch.phase = 0.0f;
+				if (voices[t].volume == 1.0f)
+					voices[t].pitch.phase = 0.0f;
 			}
 		}
 		
@@ -205,28 +225,36 @@ public:
 					float adsr = v->a.process();
 					float c = v->powtab[int(adsr * float(POWTABSIZE-1))];
 					*b++ += v->s.envprocess(v->curtab[int(v->phase)%TABSIZE],c,1) * vol * adsr;
-          if (v->pitch.amp) {
-            float rp = v->pitch.amp * sintab[int(v->pitch.phase)%TABSIZE];
-            float scale = pow(2.0f, rp / 12.0f);
-            v->phase += v->phasestep * scale;
-          } else {					
-            v->phase += v->phasestep;
-          }
+					// pitch sliding
+					if (((v->slide_step > 0) && (v->phasestep < v->end_phasestep)) ||   
+					((v->slide_step < 0) && (v->phasestep > v->end_phasestep))) { 
+						v->phasestep += v->slide_step;
+					} else {
+						v->phasestep = v->end_phasestep;
+					}
+					// pitch lfo
+					if (v->pitch.amp) {
+						float rp = v->pitch.amp * sintab[int(v->pitch.phase)%TABSIZE];
+						float scale = pow(2.0f, rp / 12.0f);
+						v->phase += v->phasestep * scale;
+					} else {
+						v->phase += v->phasestep;
+					}
 					while (v->phase > TABSIZE)
 						v->phase -= TABSIZE;
-          v->pitch.phase += v->pitch.phasestep;
-          while (v->pitch.phase > TABSIZE)
-            v->pitch.phase -= TABSIZE;
+					v->pitch.phase += v->pitch.phasestep;
+					while (v->pitch.phase > TABSIZE)
+						v->pitch.phase -= TABSIZE;
 				}
 			} else {
-        v->pitch.phase += v->pitch.phasestep * (float)n;
-        while (v->pitch.phase > TABSIZE)
-          v->pitch.phase -= TABSIZE;
-      }
+				v->pitch.phase += v->pitch.phasestep * (float)n;
+				while (v->pitch.phase > TABSIZE)
+					v->pitch.phase -= TABSIZE;
+			}
 		}
 		dsp_amp(outL, n, amp); 
 		dsp_clip(outL, n, 1);
-		dsp_copy(outL, outR, n);		
+		dsp_copy(outL, outR, n);
 	}
 };
 
