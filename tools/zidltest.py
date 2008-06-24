f47f53f6478f4c8061033fca6e4ae661976b3f68
paniq <none@none>
2008-06-24 22:17:12 +0000

    zidl can do doc strings, various basic types, class references, in/out parameters, namespaces, c-header output

--- tools/zidltest.py
+++ tools/zidltest.py
@@ -5,15 +5,27 @@
 import os
 
 idl_data = """
+
+include "stdio.h"
+
 namespace zzub:
 
-	class plugin:
-		def get_name(): (str name)
-		def set_name(str name)
+	class Plugin:
+		def get_name(): string
+		def set_name(string name)
+		def get_enabled(): bool
+		def get_position(): float x, float y
 
-	class player:
-		def get_name(): (str name)
-		def set_name(str name)
+	class Player:
+		"here is a docstring for get_name(). if this string extends"
+		"over multiple rows, it's going to be merged into"
+		"one huge section."
+		def get_name(): string
+		
+		"here is another one."
+		def set_name(string name)
+		
+		def get_plugin(string name): Plugin first, Plugin second
 """
 
 idl_file = "test.zidl"
@@ -21,7 +33,7 @@ idl_file = "test.zidl"
 file(idl_file,"w").write(idl_data)
 
 ret = os.system('./zidl --c-header test_zidl.h ' + idl_file)
-print ret
+assert ret == 0
 
 data = file("test_zidl.h","r").read()
 print data
