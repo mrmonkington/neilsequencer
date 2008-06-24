b0c6a38046e935494bd500529cdc4482437f1d1c
paniq <none@none>
2008-06-24 22:38:45 +0000

    added example libzzub.zidl

--- tools/zidltest.py
+++ tools/zidltest.py
@@ -4,35 +4,7 @@
 
 import os
 
-idl_data = """
-
-include "stdio.h"
-
-namespace zzub:
-
-	class Plugin:
-		def get_name(): string
-		def set_name(string name)
-		def get_enabled(): bool
-		def get_position(): float x, float y
-
-	class Player:
-		"here is a docstring for get_name(). if this string extends"
-		"over multiple rows, it's going to be merged into"
-		"one huge section."
-		def get_name(): string
-		
-		"here is another one."
-		def set_name(string name)
-		
-		def get_plugin(string name): Plugin first, Plugin second
-"""
-
-idl_file = "test.zidl"
-
-file(idl_file,"w").write(idl_data)
-
-ret = os.system('./zidl --c-header test_zidl.h ' + idl_file)
+ret = os.system('./zidl --c-header test_zidl.h libzzub.zidl')
 assert ret == 0
 
 data = file("test_zidl.h","r").read()
