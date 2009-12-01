4cd5214d89cc846d403f9c6c755f55398fc4dd9c
vytas@vytas-desktop <vytas@vytas-desktop>
2009-12-01 03:54:41 +0200

    Removed ld fungus

--- src/plugins/ld_fungus/Fft.h
+++
@@ -1,9 +0,0 @@
-#ifndef __FFT_H_
-#define __FFT_H_
-
-void fft(float *re, float *im, int m);
-void inverse_fft(float *re, float *im, int m);
-void real_fft(float *re, float *im, int m);
-void inverse_real_fft(float *re, float *im, int m);
-
-#endif
--- src/plugins/ld_fungus/fungus.h
+++
@@ -1,9 +0,0 @@
-
-struct fungus_machine_info : zzub::info {
-	fungus_machine_info();
-	virtual zzub::plugin* create_plugin() const;
-	virtual bool store_info(zzub::archive *data) const;
-};
-
-extern fungus_machine_info fungus_info;
-
