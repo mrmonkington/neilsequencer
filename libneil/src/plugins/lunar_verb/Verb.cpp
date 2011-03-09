ac34c6a3b4f7e43b74c2777a67e345ea98c990bb
Vytautas Jancauskas <unaudio@gmail.com>
2011-03-09 14:58:37 +0200

    Added lunar verb stub

---
+++ libneil/src/plugins/lunar_verb/Verb.cpp
@@ -0,0 +1,207 @@
+#include <algorithm>
+#include <cstdio>
+
+#include "Delay.hpp"
+
+Svf::Svf() {
+  fs = 44100;
+  fc = 523;
+  res = 0;
+  drive = 0;
+  reset();
+}
+	
+void Svf::reset() {
+  memset(v, 0, sizeof(float) * 5);
+}
+	
+void Svf::setup(float fs, float fc, float res, float drive) {
+  fs = fs;
+  fc = fc;
+  res = res;
+  drive = drive;
+  freq = 2.0 * sin(M_PI * std::min(0.25f, fc / (fs * 2)));
+  damp = std::min(float(2.0f * (1.0f - pow(res, 0.25f))), 
+		  std::min(2.0f, 2.0f / freq - freq * 0.5f));
+}
+
+float Svf::sample(float sample, int mode) {
+  float in, out;
+  // To make it possible to select only low, high and band (hackish).
+  mode = mode + 1;
+  in = sample;
+  v[0] = (in - damp * v[3]);
+  v[1] = (v[1] + freq * v[3]);
+  v[2] = (v[0] - v[1]);
+  v[3] = (freq * v[2] + v[3] - drive * v[3] * v[3] * v[3]);
+  out = 0.5 * v[mode];
+  v[0] = (in - damp * v[3]);
+  v[1] = (v[1] + freq * v[3]);
+  v[2] = (v[0] - v[1]);
+  v[3] = (freq * v[2] + v[3] - drive * v[3] * v[3] * v[3]);
+  out += 0.5 * v[mode];
+  return out;    
+}
+
+float LunarDelay::squash(float x) {
+  if (x >= 1.0) {
+    return 1.0;
+  } else if (x <= -1.0) {
+    return -1.0;
+  } else {
+    return 1.5 * x - 0.5 * x * x * x;
+  }
+}
+
+void LunarDelay::rb_init(ringbuffer_t *rb) {
+  for (int i = 0; i < MAX_DELAY_LENGTH; i++) {
+    rb->buffer[i] = 0.0f;
+  }
+  rb->eob = rb->buffer + 1;
+  rb->pos = rb->buffer;
+}
+
+void LunarDelay::rb_setup(ringbuffer_t *rb, int size) {
+  rb->eob = rb->buffer + size;
+  while (rb->pos >= rb->eob)
+    rb->pos -= size;
+}
+
+void LunarDelay::rb_mix(ringbuffer_t *rb, Svf *filter, float *out, int n) {
+  float s;
+  while (n--) {
+    s = *out;
+    *out = (*out * dry) + (*rb->pos * wet);
+    *rb->pos = std::min(std::max((*rb->pos * fb) + s, -1.0f), 1.0f);
+    *rb->pos = filter->sample(*rb->pos, filter_mode);
+    *rb->pos = squash(*rb->pos);
+    out++;
+    rb->pos++;
+    if (rb->pos == rb->eob) // wrap
+      rb->pos = rb->buffer;
+  }
+}
+
+LunarDelay::LunarDelay() {
+  global_values = &gval;
+  attributes = 0;
+  track_values = 0;
+}
+
+void LunarDelay::init(zzub::archive *pi) {
+  rb_init(&rb[0]);
+  rb_init(&rb[1]);
+  wet = 0.0f;
+  dry = 0.0f;
+  fb = 0.0f;
+}
+	
+void LunarDelay::update_buffer() {
+  ldelay = ldelay_ticks * _master_info->samples_per_tick;
+  rdelay = rdelay_ticks * _master_info->samples_per_tick;
+  int rbsizel = std::min((int)ldelay, MAX_DELAY_LENGTH);
+  int rbsizer = std::min((int)rdelay, MAX_DELAY_LENGTH);
+  rb_setup(&rb[0], rbsizel);
+  rb_setup(&rb[1], rbsizer);
+}
+	
+void LunarDelay::process_events() {
+  int update = 0;
+  if (gval.l_delay_ticks != 0xffff) {
+    ldelay_ticks = 0.25 + 
+      (gval.l_delay_ticks / (float)para_l_delay_ticks->value_max) * 
+      (32.0 - 0.25);
+  }
+  if (gval.r_delay_ticks != 0xffff) {
+    rdelay_ticks = 0.25 +
+      (gval.r_delay_ticks / (float)para_r_delay_ticks->value_max) *
+      (32.0 - 0.25);
+  }
+  if (gval.filter_mode != 0xff) {
+    filter_mode = gval.filter_mode;
+    update = 1;
+  }
+  if (gval.cutoff != 0xffff) {
+    cutoff = (float)gval.cutoff;
+    update = 1;
+  }
+  if (gval.resonance != 0xffff) {
+    resonance = gval.resonance / (float)para_resonance->value_max;
+    update = 1;
+  }
+  if (gval.wet != 0xffff) {
+    float val = -48.0 + (gval.wet / (float)para_wet->value_max) * 60.0;
+    wet = dbtoamp(val, -48.0f);
+  }
+  if (gval.dry != 0xffff) {
+    float val = -48.0 + (gval.dry / (float)para_dry->value_max) * 60.0;
+    dry = dbtoamp(val, -48.0f);
+  }
+  if (gval.fb != 0xffff) {
+    float val = -48.0 + (gval.fb / (float)para_fb->value_max) * 60.0;
+    fb = dbtoamp(val, -48.0f);
+  }
+  if (update == 1) {
+    int srate = _master_info->samples_per_second;
+    for (int i = 0; i < 2; i++) {
+      filters[i].setup((float)srate, this->cutoff, this->resonance, 0.0);
+    }
+  }
+}
+
+bool LunarDelay::process_stereo(float **pin, float **pout, int n, int mode) {
+  update_buffer();
+  for (int i = 0; i < n; i++) {
+    pout[0][i] = pin[0][i];
+    pout[1][i] = pin[1][i];
+  }
+  rb_mix(&rb[0], &filters[0], pout[0], n);
+  rb_mix(&rb[1], &filters[1], pout[1], n);
+  return true;
+}
+
+const char *LunarDelay::describe_value(int param, int value) {
+  static char txt[20];
+  switch(param) {
+  case 0:
+    sprintf(txt, "%.2f ticks", ldelay_ticks);
+    break;
+  case 1:
+    sprintf(txt, "%.2f ticks", rdelay_ticks);
+    break;
+  case 2:
+    switch(filter_mode) {
+    case 0:
+      sprintf(txt, "Low");
+      break;
+    case 1:
+      sprintf(txt, "High");
+      break;
+    case 2:
+      sprintf(txt, "Band");
+      break;
+    default:
+      sprintf(txt, "???");
+      break;
+    }
+    break;
+  case 3:
+    sprintf(txt, "%d Hz", (int)cutoff);
+    break;
+  case 4:
+    sprintf(txt, "%.2f", resonance);
+    break;
+  case 5:
+    sprintf(txt, "%.2f", 10.0 * log10(fb));
+    break;
+  case 6:
+    sprintf(txt, "%.2f", 10.0 * log10(wet));
+    break;
+  case 7:
+    sprintf(txt, "%.2f", 10.0 * log10(dry));
+    break;
+  default:
+    return 0;
+  }
+  return txt;
+}
---
+++ libneil/src/plugins/lunar_verb/Verb.hpp
@@ -0,0 +1,232 @@
+#ifndef LUNAR_DELAY_HPP
+#define LUNAR_DELAY_HPP
+
+#define MAX_DELAY_LENGTH 192000 // in samples
+
+#include <stdint.h>
+#include <cmath>
+#include <cstring>
+
+#include <zzub/signature.h>
+#include <zzub/plugin.h>
+
+struct Gvals {
+  uint16_t l_delay_ticks;
+  uint16_t r_delay_ticks;
+  uint8_t filter_mode;
+  uint16_t cutoff;
+  uint16_t resonance;
+  uint16_t fb;
+  uint16_t wet;
+  uint16_t dry;
+} __attribute__((__packed__));
+
+const zzub::parameter *para_l_delay_ticks = 0;
+const zzub::parameter *para_r_delay_ticks = 0;
+const zzub::parameter *para_filter_mode = 0;
+const zzub::parameter *para_cutoff = 0;
+const zzub::parameter *para_resonance = 0;
+const zzub::parameter *para_fb = 0;
+const zzub::parameter *para_wet = 0;
+const zzub::parameter *para_dry = 0;
+
+const char *zzub_get_signature() { 
+  return ZZUB_SIGNATURE; 
+}
+
+class Svf {
+private:
+  float fs;
+  float fc;
+  float res;
+  float drive;
+  float freq;
+  float damp;
+  float v[5];
+public:
+  Svf();
+  void reset();
+  void setup(float fs, float fc, float res, float drive);
+  float sample(float sample, int mode);
+};
+
+class LunarDelay : public zzub::plugin {
+private:
+  Gvals gval;
+  struct ringbuffer_t {
+    float buffer[MAX_DELAY_LENGTH]; // ringbuffer
+    float *eob; // end of buffer
+    float *pos; // buffer position
+  };
+  ringbuffer_t rb[2];
+  float ldelay_ticks, rdelay_ticks;
+  float ldelay;
+  float rdelay;
+  float wet;
+  float dry;
+  float fb;
+  int mode, filter_mode;
+  float cutoff, resonance;
+  Svf filters[2];
+  inline float dbtoamp(float db, float limit) {
+    if (db <= limit)
+      return 0.0f;
+    return pow(10.0f, db / 20.0f);
+  }
+  float squash(float x);
+  void rb_init(ringbuffer_t *rb);
+  void rb_setup(ringbuffer_t *rb, int size);
+  void rb_mix(ringbuffer_t *rb, Svf *filter, float *out, int n);
+  void update_buffer();
+public:
+  LunarDelay();
+  virtual ~LunarDelay() {}
+  virtual void init(zzub::archive* pi);
+  virtual void process_events();
+  virtual bool process_stereo(float **pin, float **pout, 
+			      int numsamples, int mode);
+  virtual bool process_offline(float **pin, float **pout, 
+			       int *numsamples, int *channels, 
+			       int *samplerate) { return false; }
+  virtual const char * describe_value(int param, int value); 
+  virtual void process_controller_events() {}
+  virtual void destroy() {}
+  virtual void stop() {}
+  virtual void load(zzub::archive *arc) {}
+  virtual void save(zzub::archive*) {}
+  virtual void attributes_changed() {}
+  virtual void command(int) {}
+  virtual void set_track_count(int) {}
+  virtual void mute_track(int) {}
+  virtual bool is_track_muted(int) const { return false; }
+  virtual void midi_note(int, int, int) {}
+  virtual void event(unsigned int) {}
+  virtual const zzub::envelope_info** get_envelope_infos() { return 0; }
+  virtual bool play_wave(int, int, float) { return false; }
+  virtual void stop_wave() {}
+  virtual int get_wave_envelope_play_position(int) { return -1; }
+  virtual const char* describe_param(int) { return 0; }
+  virtual bool set_instrument(const char*) { return false; }
+  virtual void get_sub_menu(int, zzub::outstream*) {}
+  virtual void add_input(const char*, zzub::connection_type) {}
+  virtual void delete_input(const char*, zzub::connection_type) {}
+  virtual void rename_input(const char*, const char*) {}
+  virtual void input(float**, int, float) {}
+  virtual void midi_control_change(int, int, int) {}
+  virtual bool handle_input(int, int, int) { return false; }
+  virtual void process_midi_events(zzub::midi_message* pin, int nummessages) {}
+  virtual void get_midi_output_names(zzub::outstream *pout) {}
+  virtual void set_stream_source(const char* resource) {}
+  virtual const char* get_stream_source() { return 0; }
+  virtual void play_pattern(int index) {}
+  virtual void configure(const char *key, const char *value) {}
+};
+
+struct LunarDelayInfo : zzub::info {
+  LunarDelayInfo() {
+    this->flags = 
+      zzub::plugin_flag_has_audio_input | zzub::plugin_flag_has_audio_output;
+    this->name = "Lunar Delay";
+    this->short_name = "Delay";
+    this->author = "SoMono";
+    this->uri = "@trac.zeitherrschaft.org/aldrin/lunar/effect/delay;1";
+    para_l_delay_ticks = &add_global_parameter()
+      .set_word()
+      .set_name("Delay L")
+      .set_description("Left channel delay in ticks")
+      .set_value_min(0)
+      .set_value_max(127)
+      .set_value_none(0xffff)
+      .set_value_default(11)
+      .set_state_flag();
+    para_r_delay_ticks = &add_global_parameter()
+      .set_word()
+      .set_name("Delay R")
+      .set_description("Right channel delay in ticks")
+      .set_value_min(0)
+      .set_value_max(127)
+      .set_value_none(0xffff)
+      .set_value_default(11)
+      .set_state_flag();
+    para_filter_mode = &add_global_parameter()
+      .set_byte()
+      .set_name("Filter Mode")
+      .set_description("Choose between low-pass, high-pass and band-pass modes")
+      .set_value_min(0)
+      .set_value_max(2)
+      .set_value_none(0xff)
+      .set_value_default(0)
+      .set_state_flag();
+    para_cutoff = &add_global_parameter()
+      .set_word()
+      .set_name("Cutoff")
+      .set_description("Filter cutoff frequency")
+      .set_value_min(20)
+      .set_value_max(20000)
+      .set_value_none(0xffff)
+      .set_value_default(10000)
+      .set_state_flag();
+    para_resonance = &add_global_parameter()
+      .set_word()
+      .set_name("Resonance")
+      .set_description("Filter resonance")
+      .set_value_min(0x0000)
+      .set_value_max(0xfffe)
+      .set_value_none(0xffff)
+      .set_value_default(0x0000)
+      .set_state_flag();
+    para_fb = &add_global_parameter()
+      .set_word()
+      .set_name("Feedback")
+      .set_description("Gain of feedback signal")
+      .set_value_min(0)
+      .set_value_max(6000)
+      .set_value_none(0xffff)
+      .set_value_default(4000)
+      .set_state_flag();
+    para_wet = &add_global_parameter()
+      .set_word()
+      .set_name("Wet Gain")
+      .set_description("Gain of delayed signal")
+      .set_value_min(0)
+      .set_value_max(6000)
+      .set_value_none(0xffff)
+      .set_value_default(4800)
+      .set_state_flag();
+    para_dry = &add_global_parameter()
+      .set_word()
+      .set_name("Dry Gain")
+      .set_description("Gain of dry signal")
+      .set_value_min(0)
+      .set_value_max(6000)
+      .set_value_none(0xffff)
+      .set_value_default(4800)
+      .set_state_flag();
+  }
+  virtual zzub::plugin* create_plugin() const { return new LunarDelay(); }
+  virtual bool store_info(zzub::archive *data) const { return false; }
+} MachineInfo;
+
+struct LunarDelay_PluginCollection : zzub::plugincollection {
+  virtual void initialize(zzub::pluginfactory *factory) {
+    factory->register_info(&MachineInfo);
+  }
+  virtual const zzub::info *get_info(const char *uri, zzub::archive *data) { 
+    return 0; 
+  }
+  virtual void destroy() { 
+    delete this; 
+  }
+  virtual const char *get_uri() { 
+    return 0;
+  }
+  virtual void configure(const char *key, const char *value) {
+
+  }
+};
+
+zzub::plugincollection *zzub_get_plugincollection() {
+  return new LunarDelay_PluginCollection();
+}
+
+#endif // LUNAR_DELAY_HPP
