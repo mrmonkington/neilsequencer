8a397b3e4cb108f5d5086c0238e8faa488048a9a
Vytautas Jancauskas <unaudio@gmail.com>
2011-03-11 10:38:19 +0200

    Oops forgot to add bitcrusher files

---
+++ libneil/src/plugins/lunar_bitcrusher/BitCrusher.cpp
@@ -0,0 +1,61 @@
+#include <algorithm>
+#include <cstdio>
+
+#include "BitCrusher.hpp"
+
+LunarBitCrusher::LunarBitCrusher() {
+  global_values = &gval;
+  attributes = 0;
+  track_values = 0;
+}
+
+void LunarBitCrusher::init(zzub::archive *pi) {
+  crush = 16;
+  downsample = 1;
+  sampleL = 0.0;
+  sampleR = 0.0;
+  counter = downsample;
+}
+	
+void LunarBitCrusher::process_events() {
+  if (gval.crush != 0xff) {
+    crush = gval.crush;
+  }
+  if (gval.downsample != 0xff) {
+    downsample = gval.downsample;
+  }
+}
+
+bool LunarBitCrusher::process_stereo(float **pin, float **pout, int n, int mode) {
+  for (int i = 0; i < n; i++) {
+    // Only play back every <downsample> samples.
+    if (--counter == 0) {
+      sampleL = pin[0][i];
+      sampleR = pin[1][i];
+      counter = downsample;
+    }
+    // Bitcrush the samples.
+    int in_left = (int)round((sampleL + 1.0) * 0.5 * 65536.0);
+    int in_right = (int)round((sampleR + 1.0) * 0.5 * 65536.0);
+    int out_left = keep_bits_from_16(in_left, crush);
+    int out_right = keep_bits_from_16(in_right, crush);
+    pout[0][i] = (((float)out_left / 65536.0) - 0.5) * 2.0;
+    pout[1][i] = (((float)out_right / 65536.0) - 0.5) * 2.0;
+  } 
+  return true;
+}
+
+const char *LunarBitCrusher::describe_value(int param, int value) {
+  static char txt[20];
+  switch(param) {
+  case 0:
+    sprintf(txt, "%d bits", value);
+    break;
+  case 1:
+    sprintf(txt, "%d times", value);
+    break;
+  default:
+    return 0;
+  }
+  return txt;
+}
---
+++ libneil/src/plugins/lunar_bitcrusher/BitCrusher.hpp
@@ -0,0 +1,133 @@
+#ifndef LUNAR_BITCRUSHER_HPP
+#define LUNAR_BITCRUSHER_HPP
+
+#include <stdint.h>
+#include <cmath>
+#include <cstring>
+
+#include <zzub/signature.h>
+#include <zzub/plugin.h>
+
+struct Gvals {
+  uint8_t crush;
+  uint8_t downsample;
+} __attribute__((__packed__));
+
+const zzub::parameter *para_crush = 0;
+const zzub::parameter *para_downsample = 0;
+
+const char *zzub_get_signature() { 
+  return ZZUB_SIGNATURE; 
+}
+
+class LunarBitCrusher : public zzub::plugin {
+private:
+  Gvals gval;
+  float sampleL, sampleR;
+  int crush, downsample, counter;
+  inline int keep_bits_from_16(int input, int keep_bits) {
+    // Only keep the last 16 bits.
+    input &= 0xffff;
+    input &= (-1 << (16 - keep_bits));
+    // Only keep the last 16 bits.
+    input &= 0xffff;
+    return input;
+  }
+public:
+  LunarBitCrusher();
+  virtual ~LunarBitCrusher() {}
+  virtual void init(zzub::archive* pi);
+  virtual void process_events();
+  virtual bool process_stereo(float **pin, float **pout, 
+			      int numsamples, int mode);
+  virtual bool process_offline(float **pin, float **pout, 
+			       int *numsamples, int *channels, 
+			       int *samplerate) { return false; }
+  virtual const char * describe_value(int param, int value); 
+  virtual void process_controller_events() {}
+  virtual void destroy() {}
+  virtual void stop() {}
+  virtual void load(zzub::archive *arc) {}
+  virtual void save(zzub::archive*) {}
+  virtual void attributes_changed() {}
+  virtual void command(int) {}
+  virtual void set_track_count(int) {}
+  virtual void mute_track(int) {}
+  virtual bool is_track_muted(int) const { return false; }
+  virtual void midi_note(int, int, int) {}
+  virtual void event(unsigned int) {}
+  virtual const zzub::envelope_info** get_envelope_infos() { return 0; }
+  virtual bool play_wave(int, int, float) { return false; }
+  virtual void stop_wave() {}
+  virtual int get_wave_envelope_play_position(int) { return -1; }
+  virtual const char* describe_param(int) { return 0; }
+  virtual bool set_instrument(const char*) { return false; }
+  virtual void get_sub_menu(int, zzub::outstream*) {}
+  virtual void add_input(const char*, zzub::connection_type) {}
+  virtual void delete_input(const char*, zzub::connection_type) {}
+  virtual void rename_input(const char*, const char*) {}
+  virtual void input(float**, int, float) {}
+  virtual void midi_control_change(int, int, int) {}
+  virtual bool handle_input(int, int, int) { return false; }
+  virtual void process_midi_events(zzub::midi_message* pin, int nummessages) {}
+  virtual void get_midi_output_names(zzub::outstream *pout) {}
+  virtual void set_stream_source(const char* resource) {}
+  virtual const char* get_stream_source() { return 0; }
+  virtual void play_pattern(int index) {}
+  virtual void configure(const char *key, const char *value) {}
+};
+
+struct LunarBitCrusherInfo : zzub::info {
+  LunarBitCrusherInfo() {
+    this->flags = 
+      zzub::plugin_flag_has_audio_input | zzub::plugin_flag_has_audio_output;
+    this->name = "Lunar BitCrusher";
+    this->short_name = "BitCrusher";
+    this->author = "SoMono";
+    this->uri = "@neil/lunar/effect/bitcrusher;1";
+    para_crush = &add_global_parameter()
+      .set_byte()
+      .set_name("Crush")
+      .set_description("Amount of bitcrushing applied")
+      .set_value_min(1)
+      .set_value_max(16)
+      .set_value_none(0xff)
+      .set_value_default(16)
+      .set_state_flag();
+    para_downsample = &add_global_parameter()
+      .set_byte()
+      .set_name("Downsample")
+      .set_description("How many times to reduce the sampling rate")
+      .set_value_min(1)
+      .set_value_max(128)
+      .set_value_none(0xff)
+      .set_value_default(1)
+      .set_state_flag();
+  }
+  virtual zzub::plugin* create_plugin() const { return new LunarBitCrusher(); }
+  virtual bool store_info(zzub::archive *data) const { return false; }
+} MachineInfo;
+
+struct LunarBitCrusher_PluginCollection : zzub::plugincollection {
+  virtual void initialize(zzub::pluginfactory *factory) {
+    factory->register_info(&MachineInfo);
+  }
+  virtual const zzub::info *get_info(const char *uri, zzub::archive *data) { 
+    return 0; 
+  }
+  virtual void destroy() { 
+    delete this; 
+  }
+  virtual const char *get_uri() { 
+    return 0;
+  }
+  virtual void configure(const char *key, const char *value) {
+
+  }
+};
+
+zzub::plugincollection *zzub_get_plugincollection() {
+  return new LunarBitCrusher_PluginCollection();
+}
+
+#endif // LUNAR_BITCRUSHER_HPP
