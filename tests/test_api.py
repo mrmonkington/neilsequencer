06d54bee18b74d923686d1880767869cd2d7ba01
bucket_brigade <none@none>
2013-05-10 20:19:34 +0300

    Added checks to SConscript files to see if correct version of portaudio is available and removed some now irrelevant options from build system
    --HG--
    branch : portaudio

--- tests/test_api.py
+++ tests/test_api.py
@@ -4,16 +4,24 @@ import unittest
 import zzub
 import ctypes
 
+######################################
+# This tests PortAudio playback driver
+######################################
+
 class TestPortAudio(unittest.TestCase):
     def setUp(self):
         self.player = zzub.zzub_player_create()
         self.driver = zzub.zzub_audiodriver_create_portaudio(self.player)
 
     def testGetCount(self):
+        """Check if the number of devices is not zero.
+        """
         ndevices = zzub.zzub_audiodriver_get_count(self.driver)
         self.assertNotEqual(ndevices, 0)
 
     def testGetName(self):
+        """Check if zzub_audiodriver_get_name returns a proper string.
+        """
         ndevices = zzub.zzub_audiodriver_get_count(self.driver)
         for i in range(ndevices):
             name = (ctypes.c_char * 1024)()
@@ -21,6 +29,8 @@ class TestPortAudio(unittest.TestCase):
             self.assertGreater(len(ctypes.string_at(name)), 0)
 
     def testGetSampleRates(self):
+        """Check if the device sample rate list is not empty.
+        """
         ndevices = zzub.zzub_audiodriver_get_count(self.driver)
         maxrates = 20
         for i in range(ndevices):
